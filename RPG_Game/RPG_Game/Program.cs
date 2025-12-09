b90435c787087f3e41cb3b34edf660151f7d2bb6
Vlad368235 <honor8s161119@gmail.com>
2025-12-09 12:34:13 +0200

    Add class Item

--- RPG_Game/RPG_Game/Program.cs
+++ RPG_Game/RPG_Game/Program.cs
@@ -8,6 +8,25 @@ namespace RPG_Game
         void Equip();
         void Unequip();
     }
+
+    public class Player
+    { 
+    
+    }
+    public abstract class Item
+    { 
+        public string Name { get; protected set; }
+        public string Description { get; protected set; }
+
+        protected Item(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        public abstract void Use(Player player);
+    }
+
     internal class Program
     {
         static void Main(string[] args)
