5af9979268f51eecad7206549f4ccb2a0caf47ff
Vlad368235 <honor8s161119@gmail.com>
2025-12-16 12:59:14 +0200

    add healthpotion

--- RPG_Game/RPG_Game/Program.cs
+++ RPG_Game/RPG_Game/Program.cs
@@ -9,6 +9,22 @@ namespace RPG_Game
         }
     }
 
+    public class HealthPotion : Item
+    {
+        private int _healthAmount;
+
+        public HealthPotion() : base("Зілля здоров'я", "Відновлює 40 HP")
+        {
+            _healthAmount = 40;
+        }
+
+        public override void Use(Player player)
+        {
+            Console.WriteLine($"{player.Name} використовує {Name}");
+            player.Heal(_healthAmount);
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
