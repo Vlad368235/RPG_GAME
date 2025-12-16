f296d7283ee83d033d6c74b106c5cae0e30de14e
Vlad368235 <honor8s161119@gmail.com>
2025-12-16 13:05:40 +0200

    Goblin

--- RPG_Game/RPG_Game/Program.cs
+++ RPG_Game/RPG_Game/Program.cs
@@ -25,6 +25,21 @@ namespace RPG_Game
         }
     }
 
+    public class Goblin : Enemy
+    {
+        public Goblin() : base("Гоблін", 40, 8, 30)
+        {
+            AddLoot(new HealthPotion());
+        }
+
+        public override void Attack(Character target)
+        {
+            var damage = Strength;
+            Console.WriteLine($"{Name} б'є кинджалом і завдає {damage}");
+            target.TakeDamage(damage);
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
